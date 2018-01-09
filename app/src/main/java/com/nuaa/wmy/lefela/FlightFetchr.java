6f4d1e2bc5ce52e20b91ac3baeff35b9c4756172
wmy <wmy0828@nuaa.edu.cn>
2018-01-09 22:53:25 +0800

    Second commit

---
+++ app/src/main/java/com/nuaa/wmy/lefela/FlightFetchr.java
@@ -0,0 +1,180 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+/**
+ * Make an HTTP request to the given URL and return a String as a response.
+ */
+
+public class FlightFetchr {
+    private String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.connect();
+            inputStream = urlConnection.getInputStream();
+            jsonResponse = readFromStream(inputStream);
+        } catch (IOException e) {
+
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // function must handle java.io.IOException here
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+}
+
+    private Event extractFeatureFromJson(String earthquakeJSON) {
+        try {
+            JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
+            JSONArray featureArray = baseJsonResponse.getJSONArray("features");
+
+            // If there are results in the features array
+            if (featureArray.length() > 0) {
+                // Extract out the first feature (which is an earthquake)
+                JSONObject firstFeature = featureArray.getJSONObject(0);
+                JSONObject properties = firstFeature.getJSONObject("properties");
+
+                // Extract out the title, time, and tsunami values
+                String title = properties.getString("title");
+                long time = properties.getLong("time");
+                int tsunamiAlert = properties.getInt("tsunami");
+
+                // Create a new {@link Event} object
+                return new Event(title, time, tsunamiAlert);
+            }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Problem parsing the earthquake JSON results", e);
+        }
+        return null;
+    }
+}
+
+private class TsunamiAsyncTask extends AsyncTask<URL, Void, Event> {
+
+    @Override
+    protected Event doInBackground(URL... urls) {
+        // Create URL object
+        URL url = createUrl(USGS_REQUEST_URL);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = "";
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            // TODO Handle the IOException
+        }
+
+        // Extract relevant fields from the JSON response and create an {@link Event} object
+        Event earthquake = extractFeatureFromJson(jsonResponse);
+
+        // Return the {@link Event} object as the result fo the {@link TsunamiAsyncTask}
+        return earthquake;
+    }
+
+    /**
+     * Update the screen with the given earthquake (which was the result of the
+     * {@link TsunamiAsyncTask}).
+     */
+    @Override
+    protected void onPostExecute(Event earthquake) {
+        if (earthquake == null) {
+            return;
+        }
+
+        updateUi(earthquake);
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException exception) {
+            Log.e(LOG_TAG, "Error with creating URL", exception);
+            return null;
+        }
+        return url;
+    }
+    private class FlightAsyncTask extends AsyncTask<URL, Void, Event> {
+
+        @Override
+        protected Event doInBackground(URL... urls) {
+            // Create URL object
+            URL url = createUrl(USGS_REQUEST_URL);
+
+            // Perform HTTP request to the URL and receive a JSON response back
+            String jsonResponse = "";
+            try {
+                jsonResponse = makeHttpRequest(url);
+            } catch (IOException e) {
+                // TODO Handle the IOException
+            }
+
+            // Extract relevant fields from the JSON response and create an {@link Event} object
+            Event earthquake = extractFeatureFromJson(jsonResponse);
+
+            // Return the {@link Event} object as the result fo the {@link TsunamiAsyncTask}
+            return earthquake;
+        }
+
+        /**
+         * Update the screen with the given earthquake (which was the result of the
+         * {@link TsunamiAsyncTask}).
+         */
+        @Override
+        protected void onPostExecute(Event earthquake) {
+            if (earthquake == null) {
+                return;
+            }
+
+            updateUi(earthquake);
+        }
+
+        /**
+         * Returns new URL object from the given string URL.
+         */
+        private URL createUrl(String stringUrl) {
+            URL url = null;
+            try {
+                url = new URL(stringUrl);
+            } catch (MalformedURLException exception) {
+                Log.e(LOG_TAG, "Error with creating URL", exception);
+                return null;
+            }
+            return url;
+        }
---
+++ app/src/main/java/com/nuaa/wmy/lefela/FlightList.java
@@ -0,0 +1,17 @@
+/**
+ * Flight datalist
+ */
+
+public class FlightList {
+    private String "depart_date": "2014-10-04",
+            "destination": "HKT",
+            "distance": 8015,
+            "found_at": "2014-09-09T14:16:07+04:00",
+            "number_of_changes": 2,
+            "origin": "LED",
+            "return_date": "2014-10-15",
+            "show_to_affiliates": false,
+            "trip_class": 0,
+            "value": 26197
+}
+}
